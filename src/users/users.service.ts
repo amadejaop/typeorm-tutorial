9c3247de75e05da6c4813c464f599d23c2e2523b
Amadeja <obsteter.amadeja@gmail.com>
2024-01-18 16:18:01 +0100

    Create user.dto file and implement CRUD
    Created create-user.dto file and implemented CRUD inside
    users.service.ts file.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor( @InjectRepository(User) private usersRepository: Repository<User>, ) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    // creates an object ready to be saved into a db table
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
   }
 
   findAll() {
-    return `This action returns all users`;
+    // SELECT * FROM user
+    return this.usersRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.usersRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    // returns query result, but not the updated user (with api's you want to return the updated users)
+    // return this.usersRepository.update(id, updateUserDto);
+
+    // recommended way
+    const user = await this.findOne(id);
+
+    // spread the user and then spread the updates
+    return this.usersRepository.save({ ...user, ...updateUserDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    // returns the removed user
+    return this.usersRepository.remove(user);
+
+    // alternative; not recommended, doesn't tell you the data that got deleted
+    // return this.usersRepository.delete(id);
   }
 }
